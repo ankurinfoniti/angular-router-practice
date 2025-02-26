b9e0434a59dda693a5a5a273da77c5808ec98073
ankurinfoniti <ankur.infoniti@gmail.com>
2025-02-26 19:08:24 +0530

    staticc page with router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CourseListComponent } from './course-list/course-list.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'courses',
+    component: CourseListComponent,
+  },
+];
