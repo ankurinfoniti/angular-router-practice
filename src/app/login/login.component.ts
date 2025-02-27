b959350ba9ba778c233b318e60d24a2ad15637f9
ankurinfoniti <ankur.infoniti@gmail.com>
2025-02-27 15:46:19 +0530

    implement simple session based login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  destroyRef = inject(DestroyRef);
+  router = inject(Router);
+  authService = inject(AuthService);
+
+  isInvalidUser = false;
+
+  onSubmit(form: NgForm) {
+    this.isInvalidUser = false;
+    if (form.valid) {
+      const email = form.value.email;
+      const password = form.value.password;
+
+      this.authService
+        .login(email, password)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe({
+          next: (resData) => {
+            if (resData) {
+              this.router.navigate(['/courses']);
+            }
+            this.isInvalidUser = true;
+          },
+          error: (errorMessage) => {
+            console.log(errorMessage);
+          },
+        });
+    } else {
+      form.form.markAllAsTouched();
+    }
+  }
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,10 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 import { User } from '../models/user.interface';
 
@@ -12,15 +16,34 @@ const API_URL = 'http://localhost:3000/';
 export class AuthService {
   http = inject(HttpClient);
 
+  login(email: string, password: string) {
+    const params = new HttpParams()
+      .set('email', email)
+      .set('password', password);
+
+    return this.http.get<User[]>(`${API_URL}users`, { params: params }).pipe(
+      map((users) => {
+        if (users.length > 0) {
+          sessionStorage.setItem('user', JSON.stringify(users[0]));
+          return users[0];
+        }
+        return null;
+      }),
+      catchError(this.handleError),
+    );
+  }
+
+  isLoggedIn() {
+    return sessionStorage.getItem('user') !== null;
+  }
+
+  logout() {
+    sessionStorage.removeItem('user');
+  }
+
   create(user: User) {
     return this.http
       .post<User>(`${API_URL}users`, user)
-      .pipe(catchError(this.handleError));
-  }
-
-  login(user: User) {
-    return this.http
-      .post<User>(`${API_URL}login`, user)
       .pipe(catchError(this.handleError));
   }
 
