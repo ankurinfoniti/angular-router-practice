e3f391dc626fd1094b4997963ff4ba773d8eeb08
ankurinfoniti <ankur.infoniti@gmail.com>
2025-02-27 11:42:06 +0530

    adding validation on register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
+import { PasswordMatchDirective } from '../core/validation/password-match.directive';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, PasswordMatchDirective],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.status);
+      form.resetForm();
+    } else {
+      form.form.markAllAsTouched();
+    }
+  }
+}
